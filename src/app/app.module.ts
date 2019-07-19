77284f104df3c3a777767595c32bb775b96264d2
desperateCoder <echotodevnull@gmail.com>
2019-07-19 20:31:14 +0200

    moved occupy-slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OccupySliderComponent } from './occupySlider/occupy-slider/occupy-slider.component';
+import { OccupySliderComponent } from './occupy-slider/occupy-slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OccupySliderComponent
+    OccupySliderComponent,
   ],
   imports: [
     BrowserModule,
